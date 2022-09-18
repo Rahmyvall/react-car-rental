5cde79c08a731e73c4e4aa37a56cc49fc0681e91
Rahmyvall <Rahmyvall@gmail.com>
2022-09-18 09:15:30 +0700

    proyek pertama

---
+++ src/components/UI/AboutSection.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "../../styles/about-section.css";
+import aboutImg from "../../assets/all-images/cars-img/IONIQ-1.jpg";
+
+const AboutSection = ({ aboutClass }) => {
+  return (
+    <section
+      className="about__section"
+      style={
+        aboutClass === "aboutPage"
+          ? { marginTop: "0px" }
+          : { marginTop: "280px" }
+      }
+    >
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <div className="about__section-content">
+              <h4 className="section__subtitle">About Us</h4>
+              <h2 className="section__title">Welcome to car rent service</h2>
+              <p className="section__description">
+              As soon as you arrive at the Airport or the Port of St Barts, you will benefit from a personalized VIP service. We will be waiting for you on arrival to deliver your car,, and your stay starts immediately, without further formalities.
+              Your car can be delivered to the Villa or the Hotel where you will be staying.
+              You will choose from our newest cars in our fleet to discover the wonderful places on our island.
+              You can count on us, we will be there to help you.
+
+              We are waiting for you.
+
+              Franck, Céline & Welcome Car Rental’s team
+              </p>
+
+              <div className="about__section-item d-flex align-items-center">
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  amet.
+                </p>
+
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  amet.
+                </p>
+              </div>
+
+              <div className="about__section-item d-flex align-items-center">
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  amet.
+                </p>
+
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  amet.
+                </p>
+              </div>
+            </div>
+          </Col>
+
+          <Col lg="6" md="6">
+            <div className="about__img">
+              <img src={aboutImg} alt="" className="w-100" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default AboutSection;
